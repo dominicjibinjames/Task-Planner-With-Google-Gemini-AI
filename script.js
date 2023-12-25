33c20f17c8ee452cf9e0b980026dda731f9b323c
Dominic Jibin James <dominicjibinjames007@gmail.com>
2023-12-25 21:36:20 +0530

    Updated script.js file for delete items

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
-
 let input = document.querySelector(".input input");
+let deleteTask = document.querySelector(".fa-trash-can");
+let count = 0;
 
-function addTask(task) {
+function addTask(count) {
     let lists = document.querySelector(".lists");
     let text = document.querySelector(".input input").value;
     let list = document.createElement("li");
-
+    list.setAttribute("id", `${count}`)
+    
     if(text.length<=0)
     {
         return input.style.borderBlockColor = "red";
     }
     else{
-    input.style.borderBlockColor = "lightgray";
-    list.innerText = text;
-    console.log(list);
-    lists.prepend(list);
-    clearTextValue();
+        input.style.borderBlockColor = "lightgray";
+        list.innerHTML = `${text} <i id="${count}" class="fa-regular fa-trash-can"></i>`;
+        lists.prepend(list);
+        clearTextValue();
+        
+        
+    let deleteItem = document.getElementById(`${count}`).childNodes[1];
+
+    deleteItem.addEventListener("click", function(e){
+        let target = e.target.id;
+        console.log(target);
+        let deleteList = document.getElementById(target);
+        console.log(deleteList);
+        deleteList.remove();
+    })
+
+    console.log(count);
     }
 }
 
 function clearTextValue(){
     let clearTextValue = input;
     clearTextValue.value = "";
+    count++;
 }
 
 input.addEventListener("keypress", function(e) {
     if(e.key==="Enter")
     {
         e.preventDefault();
-        addTask();
+        addTask(count);
     }
 })
