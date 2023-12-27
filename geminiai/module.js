b657ef427812ba2f13adb2818ab69b79e9ea5c5c
Dominic Jibin James <108563808+dominicjibinjames@users.noreply.github.com>
2023-12-28 00:09:05 +0530

    Update module.js

--- geminiai/module.js
+++ geminiai/module.js
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 // Fetch your API_KEY
-const API_KEY = "AIzaSyCCgii0Fwaeqhcb2-IwVtvg1wxB7M6g09A";
+const API_KEY = "";
 // Access your API key (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-pro" });
