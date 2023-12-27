5cfbdac07a5811268ff73f7979c511cb76f25b44
Dominic Jibin James <dominicjibinjames007@gmail.com>
2023-12-28 00:07:17 +0530

    added Gemini Ai to the project with edge cases

---
+++ geminiai/aiScript.js
@@ -0,0 +1,87 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+let aiBtn = document.querySelector("#aiBtn");
+
+const API_KEY = "AIzaSyCCgii0Fwaeqhcb2-IwVtvg1wxB7M6g09A";
+// Access your API key (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(API_KEY);
+
+
+
+ async function run() {
+    let lists = document.querySelector(".lists");
+    let text = document.querySelector(".input input").value;
+    let list = document.createElement("li");
+
+
+    list.setAttribute("id", `${count}`)
+    
+    if(text.length<=0)
+    {
+        return input.style.borderBlockColor = "red";
+    }
+    else{
+    
+    //Initiate Gemini Ai
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const prompt = text;
+
+    //Loading Screen
+    const loading = aiBtn.innerText = "Loading"
+     
+    //to temporarily Disable the Gen Ai Button to avoid multiple clicks
+    aiBtn.setAttribute("disabled","");
+
+    const result = await model.generateContent(prompt);
+
+
+    const response = await result.response;
+
+    let textContent;
+
+    aiBtn.innerText = "Gen AI"
+
+    try {
+        textContent = response.text();
+    } catch (error) {
+        alert("Error: Please rephrase your query and try again!")
+        aiBtn.removeAttribute("disabled","");
+        return
+    }
+   
+    list.innerHTML = `${prompt}: <br>${textContent} <i id="${count}" class="fa-regular fa-trash-can"></i> `;
+    
+    //Delete button for the created list
+    let deleteItem = list.querySelector('i');
+    deleteItem.addEventListener("click", function(e) {
+        let target = e.target.id;
+        let deleteList = document.getElementById(target);
+        deleteList.remove();
+    });
+
+
+    lists.prepend(list);
+    clearTextValue();
+
+    //to Enable the Gen Ai Button
+    aiBtn.removeAttribute("disabled","");
+}
+    
+}
+
+
+aiBtn.addEventListener("click", () => {
+    run();
+})
+
+function clearTextValue(){
+    let clearTextValue = input;
+    clearTextValue.value = "";
+    count++
+}
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let input = document.querySelector(".input input");
 let deleteTask = document.querySelector(".fa-trash-can");
+let addBtn = document.querySelector("#addBtn");
 let count = 0; //to track the ID's
 
 //to add and delete items in the list
-function addTask(count) {
+function addTask() {
     let lists = document.querySelector(".lists");
     let text = document.querySelector(".input input").value;
     let list = document.createElement("li");
@@ -16,18 +17,16 @@ function addTask(count) {
     else{
         input.style.borderBlockColor = "lightgray";
         list.innerHTML = `${text} <i id="${count}" class="fa-regular fa-trash-can"></i>`;
+
+        let deleteItem = list.querySelector('i');
+        deleteItem.addEventListener("click", function(e) {
+            let target = e.target.id;
+            let deleteList = document.getElementById(target);
+            deleteList.remove();
+        });
+
         lists.prepend(list);
         clearTextValue();
-        
-
-    //to Delete items in the List
-    let deleteItem = document.getElementById(`${count}`).childNodes[1];
-
-    deleteItem.addEventListener("click", function(e){
-        let target = e.target.id;
-        let deleteList = document.getElementById(target);
-        deleteList.remove();
-    })
     }
 }
 
@@ -37,10 +36,21 @@ function clearTextValue(){
     count++;
 }
 
+addBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    addTask();
+})
+
 input.addEventListener("keypress", function(e) {
     if(e.key==="Enter")
     {
         e.preventDefault();
-        addTask(count);
+        addTask();
     }
 })
+
+
+
+
+
+
