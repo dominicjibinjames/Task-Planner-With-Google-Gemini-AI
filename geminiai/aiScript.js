38d8f2dbcab22e8ea3b3acdc1d71c29572b3f804
Dominic Jibin James <dominicjibinjames007@gmail.com>
2024-01-14 00:22:39 +0530

    updated api keys for deployment

--- geminiai/aiScript.js
+++ geminiai/aiScript.js
@@ -2,7 +2,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 let aiBtn = document.querySelector("#aiBtn");
 
-const API_KEY = "";
+const API_KEY = "AIzaSyCCgii0Fwaeqhcb2-IwVtvg1wxB7M6g09A";
 // Access your API key (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(API_KEY);
 
